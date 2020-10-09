b243114180534963b35c55f4a56a1d0ff1789e69
Yosra Houimli <houimliyosra.b@gmail.com>
2020-10-09 10:08:05 +0200

    Ajoutez des fichiers projet.

---
+++ TP/DatatAccess/AccesDonees.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.Entities;
+
+namespace TP.DatatAccess
+{
+  public static  class AccesDonees
+    {
+        public static  List<Question> FindAll()
+        {
+            // il faut donnée le chemin de code 
+            string chaineDeConnexion = Properties.Settings.Default.ConnexionString;
+            MySqlConnection connexion = new MySqlConnection(chaineDeConnexion);
+            MySqlCommand commande = null;
+            MySqlDataReader dataReader = null;
+
+
+            try
+            {
+                //ouvrir la connexion
+                connexion.Open();
+
+                // Préparer l'envoi de la commande Envoie d'une commande 
+                string requette = "SELECT  id, enonce, multipleChoice, ordre  FROM t_question";
+                
+                commande = new MySqlCommand(requette, connexion);
+
+                //Envoyer la commande pour lire les donées à partir de la base de donées 
+                dataReader = commande.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+
+
+                }
+            }
+        }
+           
+        
+    }
+}
---
+++ TP/Entities/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP.Entities
+{
+  public  class Question
+    {
+        public int Id { get; set; }
+        public string Enonce { get; set; }
+        public bool EstChoixMultiple { get; set; }
+        public int Ordre { get; set; }
+    }
+}
